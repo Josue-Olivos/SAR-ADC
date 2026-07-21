818fafb2c1647c2aa1ce25e23bb529501191d082
Josue_Olivos <155196218+Josue-Olivos@users.noreply.github.com>
2026-07-21 15:08:15 -0600

    Fix instantiation of user_project module

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_josue_olivos_sar_adc user_project (
+  tt_um_josue_olivos_sar_adc dut (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
